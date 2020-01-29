857fa26e0b10f2e71dc58735c5ca11a379d78097
Daniel Emod Kovacs <kovacsemod@gmail.com>
2020-01-29 18:49:18 +0100

    feat: async iterators

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,70 @@
-import index from '.';
+import {
+  map,
+  filter,
+  take,
+  getValue,
+  reduce,
+  until,
+  first,
+  rest,
+  numericSequence,
+} from '.';
 
 describe(`index`, () => {
-  it(`should return null`, () => {
-    expect(index()).toBe(null);
+  it(`infinite even numbers sequence`, () => {
+    const infinite = numericSequence(0, 1);
+
+    const evens = filter((v: number) => !(v % 2), infinite);
+
+    const test = take(5, evens);
+
+    expect(getValue(test)).toStrictEqual([0, 2, 4, 6, 8]);
+  });
+
+  it(`reduce to sum numbers`, () => {
+    const infinite = numericSequence(0, 1);
+
+    const evens = filter((v: number) => !(v % 2), infinite);
+
+    const test = take(5, evens);
+
+    expect(reduce((acc: number, next: number) => acc + next, 0, test)).toBe(20);
+  });
+
+  it(`odd number until 9`, () => {
+    const infinite = numericSequence(1, 2);
+
+    const test = until(v => v === 9, infinite);
+
+    expect(getValue(test)).toStrictEqual([1, 3, 5, 7]);
+  });
+
+  it(`first number`, () => {
+    const infinite = numericSequence(1, 2);
+
+    const test = first(infinite);
+
+    expect(test).toStrictEqual(1);
+  });
+
+  it(`rest of the sequence`, () => {
+    const infinite = numericSequence(1, 2);
+
+    const skipFirst = rest(infinite);
+
+    const test = take(5, skipFirst);
+
+    expect(getValue(test)).toStrictEqual([3, 5, 7, 9, 11]);
+  });
+
+  it(`cubicPar example`, () => {
+    const cubicPar = (iterable: Iterable<number>) =>
+      filter(
+        (x: number) => x % 2 === 0,
+        map((x: number) => x * x * x, iterable),
+      );
+
+    const fiveElements = take(10, cubicPar([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+    expect(getValue(fiveElements)).toStrictEqual([8, 64, 216, 512, 1000]);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,122 @@
-export default () => null;
+export const generate = <T, R>(fn: () => Iterator<T, R>): Iterable<T> => {
+  return { [Symbol.iterator]: fn };
+};
+
+export const numericSequence = (beginning: number, step: number) => {
+  return generate(() => {
+    let i = beginning;
+    return {
+      next() {
+        const value = i;
+        i += step;
+        return { value, done: false };
+      },
+    };
+  });
+};
+
+export const map = <T, I extends Iterable<T>, R>(
+  mapper: (v: T) => R,
+  iterable: I,
+) => {
+  return generate(() => {
+    const iterator = iterable[Symbol.iterator]();
+
+    return {
+      next: () => {
+        const { done, value } = iterator.next();
+
+        return { done, value: done ? undefined : mapper(value) };
+      },
+    };
+  });
+};
+
+export const reduce = <T, I extends Iterable<T>, R>(
+  reducer: (acc: R, cur: T) => R,
+  initialValue: R,
+  iterable: I,
+) => {
+  const iterator = iterable[Symbol.iterator]();
+  let iterationResult;
+  let accumulator = initialValue;
+  do {
+    iterationResult = iterator.next();
+    accumulator = iterationResult.done
+      ? accumulator
+      : reducer(accumulator, iterationResult.value);
+  } while (!iterationResult.done);
+  return accumulator;
+};
+
+export const filter = <T, I extends Iterable<T>>(
+  fn: (v: T) => boolean,
+  iterable: I,
+) => {
+  return generate(() => {
+    const iterator = iterable[Symbol.iterator]();
+
+    return {
+      next: () => {
+        let done;
+        let value;
+        do {
+          const next = iterator.next();
+          done = next.done;
+          value = next.value;
+        } while (!done && !fn(value));
+        return { done, value };
+      },
+    };
+  });
+};
+
+export const until = <T>(fn: (v: T) => boolean, iterable: Iterable<T>) => {
+  return generate(() => {
+    const iterator = iterable[Symbol.iterator]();
+
+    return {
+      next: () => {
+        let { done, value } = iterator.next();
+        done = done || fn(value);
+        value = done ? undefined : value;
+        return { done, value };
+      },
+    };
+  });
+};
+
+export const first = <T>(iterable: Iterable<T>): T => {
+  return iterable[Symbol.iterator]().next().value;
+};
+
+export const rest = <T>(iterable: Iterable<T>) => {
+  return generate(() => {
+    const iterator = iterable[Symbol.iterator]();
+
+    iterator.next();
+    return iterator;
+  });
+};
+
+export const take = <T>(numberToTake: number, iterable: Iterable<T>) => {
+  return generate(() => {
+    const iterator = iterable[Symbol.iterator]();
+    let remainingElements = numberToTake;
+
+    return {
+      next: () => {
+        let { done, value } = iterator.next();
+
+        done = done || remainingElements <= 0;
+        value = done ? undefined : value;
+        remainingElements -= 1;
+
+        return { done, value };
+      },
+    };
+  });
+};
+
+export const getValue = <T, I extends Iterable<T>>(iterable: I) =>
+  Array.from(iterable);
